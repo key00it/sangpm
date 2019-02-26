8a52b7b384eed0da06de5aac2d906c0324924fe0
sangkaitoo <sanggkaitoo@gmail.com>
2019-02-27 01:45:43 +0700

    Project

--- js/index-js.js
+++ js/index-js.js
@@ -3,9 +3,11 @@ $(window).scroll(function() {
 	if (window.scrollY > 72) {
 		$('.navbar').eq(0).css("background", "rgb(59, 50, 139, 0.9)")
 		$('.navbar').eq(0).css("box-shadow", "0 2px 20px #222")
+      $('#go-to-top').css("opacity", "0.5")
 	} else {
 		$('.navbar').eq(0).css("background", "none")
 		$('.navbar').eq(0).css("box-shadow", "none")
+      $('#go-to-top').css("opacity", "0")
 	}
 });
 
@@ -44,3 +46,63 @@ jQuery(document).ready(function( $ ) {
 		time: 1000
 	});
 });
+
+$('#go-to-top').click(function(){ 
+      $('html,body').animate({ scrollTop: 0 }, 400);
+      return false; 
+});
+
+// mess
+
+$(document).ready(function() {
+   function detectmob() {
+      if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+   var t = {
+      delay: 125,
+      overlay: $(".fb-overlay"),
+      widget: $(".fb-widget"),
+      button: $(".fb-button")
+   };
+   setTimeout(function() {
+      $("div.fb-livechat").fadeIn()
+   }, 8 * t.delay);
+   if (!detectmob()) {
+      $(".ctrlq").on("click", function(e) {
+         e.preventDefault(), t.overlay.is(":visible") ? (t.overlay.fadeOut(t.delay), t.widget.stop().animate({
+            bottom: 0,
+            opacity: 0
+         }, 2 * t.delay, function() {
+            $(this).hide("slow"), t.button.show()
+         })) : t.button.fadeOut("medium", function() {
+            t.widget.stop().show().animate({
+               bottom: "30px",
+               opacity: 1
+            }, 2 * t.delay), t.overlay.fadeIn(t.delay)
+         })
+      })
+   }
+});
+
+// end mess
+
+//scroll top
+
+$('#go-to-top').click(function(){ 
+      $('html,body').animate({ scrollTop: 0 }, 400);
+      return false; 
+});
+
+//end scroll top
+
+$(document).ready(function() {
+   if (window.scrollY > 72) {
+      $('.navbar').eq(0).css("background", "rgb(59, 50, 139, 0.9)")
+      $('.navbar').eq(0).css("box-shadow", "0 2px 20px #222")
+     $('#go-to-top').css("opacity", "0.5")
+   }
+})
